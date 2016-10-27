6143a328dd069d460a163a7e405f8740d20ba49b
Tomplays <homeof+git@gmail.com>
2016-10-27 13:51:27 +0200

    fix routes *

--- Gulpfile.js
+++ Gulpfile.js
@@ -28,7 +28,7 @@ server.use(livereload({port: livereloadport}));
 server.use(express.static('./dist'));
 // Because I like HTML5 pushstate .. this redirects everything back to our index.html
 server.all('/*', function(req, res) {
-  res.sendfile('index.html', { root: 'dist' });
+  res.sendfile('*.html', { root: 'dist' });
 });
 
 // Dev task
